2bc0b615eefa9c2af8520f45faee88c4f6d0a199
Nick <nick@craable.com>
2023-10-23 16:50:55 +0300

    Finished tasks 5-1/2: Hide / Toggle

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+  button.addEventListener("click", () => {
+    text.hidden ? text.hidden = false : text.hidden = true
+  });
 }
